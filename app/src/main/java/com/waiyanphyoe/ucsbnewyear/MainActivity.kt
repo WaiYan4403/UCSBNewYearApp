dca884148608ad502c46b42712d280c7f5541599
Wai Yan Phyoe <waiyan4403@gmail.com>
2023-01-03 20:21:46 +0630

    Fixed Count

--- app/src/main/java/com/waiyanphyoe/ucsbnewyear/MainActivity.kt
+++ app/src/main/java/com/waiyanphyoe/ucsbnewyear/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.waiyanphyoe.ucsbnewyear
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -11,23 +12,29 @@ class MainActivity : AppCompatActivity() {
     lateinit var txtNum1:TextView
     lateinit var txtNum2:TextView
     lateinit var txtNum3:TextView
+    lateinit var btnPick:Button
     var usedNumbers = ArrayList<Int>()
+    var onPick:Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btnPick:Button = findViewById(R.id.btnPick)
+        btnPick = findViewById(R.id.btnPick)
         txtNum1 = findViewById(R.id.txtNum1)
         txtNum2 = findViewById(R.id.txtNum2)
         txtNum3 = findViewById(R.id.txtNum3)
 
         btnPick.setOnClickListener {
-            pickRandomNumber()
+            if(!onPick){
+                //btnPick.setBackgroundColor(Color.RED)
+                onPick = true
+                pickRandomNumber()
+            }
         }
     }
 
     private fun pickRandomNumber(){
-        if(usedNumbers.size >= 280){
+        if(usedNumbers.size >= 205){
             txtNum1.text = "E"
             txtNum2.text = "N"
             txtNum3.text = "D"
@@ -35,7 +42,7 @@ class MainActivity : AppCompatActivity() {
         else{
             var randNum = 0
             do {
-                randNum = Random().nextInt(281)
+                randNum = Random().nextInt(206)
             }while (usedNumbers.contains(randNum))
             usedNumbers.add(randNum)
             txtNum1.text = "-"
@@ -56,11 +63,13 @@ class MainActivity : AppCompatActivity() {
             Handler().postDelayed({
                 txtNum2.text = strNumber.get(1).toString()
                 //txtResult.text = String.format("%03d", randNum)
-            },2000)
+            },3000)
             Handler().postDelayed({
                 txtNum3.text = strNumber.get(2).toString()
+                onPick = false
+                //btnPick.setBackgroundColor(Color.BLUE)
                 //txtResult.text = String.format("%03d", randNum)
-            },3000)
+            },5000)
         }
     }
 }
